05c15d8d3279a8f08ac91abe1b9ce2b486e70196
alebor28 <alebor28@kingshigh.school.nz>
2025-08-28 15:00:39 +1200

    3.0

--- new-game-project/area_2d.gd
+++ new-game-project/area_2d.gd
@@ -17,16 +17,3 @@ func _on_body_entered(body: Node2D) -> void:
   # or Node2D, whatever your killzone is
 
 # Reference to the camera node
-var camera: Camera2D
-
-# Offset so the killzone stays a bit below the camera (adjust as needed)
-var offset_y = 200  
-
-func _ready():
-	# Find the camera node, change the path accordingly
-	camera = get_node("/root/YourScene/Player/Camera2D")
-
-func _process(delta):
-	# Make killzone follow the camera's vertical position with offset
-	var cam_pos = camera.global_position
-	global_position.y = cam_pos.y + offset_y
